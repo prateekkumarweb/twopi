9512126e3306d396fdec8fc4e7a723acaa459fc1
Prateek Kumar <me@prateek.page>
2025-01-22 18:34:21 +0400

    update deps, improve style of labels

--- twopi-web/app/routes/app/transaction/$id/index.tsx
+++ twopi-web/app/routes/app/transaction/$id/index.tsx
@@ -79,7 +79,7 @@ function RouteComponent() {
 function LabelAndValue({ label, value }: { label: string; value: string }) {
   return (
     <div className="flex flex-nowrap items-center gap-2">
-      <div className="grow font-semibold">{label}</div>
+      <div className="grow font-light">{label}</div>
       <div className="overflow-hidden text-ellipsis text-nowrap text-sm text-gray-700">
         {value}
       </div>
