c5bfd1960905b19b2f93b1144c6957a495444e29
Prateek Kumar <me@prateek.page>
2025-02-03 01:33:35 +0400

    twopi-web: remove prisma

--- twopi-web/migratedb.sh
+++ twopi-web/migratedb.sh
@@ -3,11 +3,3 @@
 mkdir -p $TWOPI_DATA_DIR/database
 
 pnpm run auth:migrate
-
-for file in $TWOPI_DATA_DIR/database/*.db
-do
-  file=$(basename $file)
-  [ -f "$TWOPI_DATA_DIR/database/$file" ] || continue
-  echo "Migrating file:$TWOPI_DATA_DIR/database/$file"
-  DATABASE_URL="file:$TWOPI_DATA_DIR/database/$file" pnpm run db:migrate
-done
