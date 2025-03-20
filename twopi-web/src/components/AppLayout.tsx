433ea1fc28045c8659163f501da71578a0a7fc14
Prateek Kumar <me@prateek.page>
2025-03-20 20:12:00 +0400

    feat: add header in app layout

--- twopi-web/src/components/AppLayout.tsx
+++ twopi-web/src/components/AppLayout.tsx
@@ -1,4 +1,8 @@
+import { Link, useRouter } from "@tanstack/solid-router";
+import { User } from "lucide-solid";
 import type { JSXElement } from "solid-js";
+import { apiClient } from "~/lib/openapi";
+import { Button } from "./ui/button";
 
 export default function Layout(props: {
   user?: {
@@ -8,10 +12,38 @@ export default function Layout(props: {
   };
   children: JSXElement;
 }) {
+  const signOut = async () => {
+    const router = useRouter();
+    const { error } = await apiClient.POST("/twopi-api/api/signout");
+    if (error) {
+      throw new Error(error);
+    }
+    await router.invalidate();
+    await router.navigate({ to: "/" });
+  };
+
   return (
     <div>
-      <h1>TwoPi Personal Finance</h1>
-      <div>{props.children}</div>
+      <header class="border-b-1 flex items-center justify-between border-gray-200 p-4">
+        <Link to="/app" class="flex items-center gap-2">
+          <img src="/2pi.svg" alt="TwoPi" class="h-8 w-8" />
+          <h1 class="text-lg font-semibold">TwoPi</h1>
+        </Link>
+        {props.user ? (
+          <div class="flex items-center gap-4">
+            <div class="flex items-center gap-2">
+              <User />
+              {props.user.name}
+            </div>
+            <div>
+              <Button onClick={signOut}>Sign out</Button>
+            </div>
+          </div>
+        ) : (
+          <div>Not signed in</div>
+        )}
+      </header>
+      <div class="p-4">{props.children}</div>
     </div>
   );
 }
