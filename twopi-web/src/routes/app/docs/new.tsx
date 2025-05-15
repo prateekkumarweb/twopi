346e95c7a5fb8f19ea687f35940f5fba22808c80
Prateek Kumar <me@prateek.page>
2025-05-15 21:46:25 +0400

    feat: support adding multiple md blocks

--- twopi-web/src/routes/app/docs/new.tsx
+++ twopi-web/src/routes/app/docs/new.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/solid-router";
 import { marked } from "marked";
-import { createResource, createSignal, Index } from "solid-js";
+import { createResource, Index } from "solid-js";
+import { createStore } from "solid-js/store";
 import { PageLayout } from "~/components/PageLayout";
+import { Button } from "~/components/ui/button";
 import { Textarea } from "~/components/ui/textarea";
 
 export const Route = createFileRoute("/app/docs/new")({
@@ -9,32 +11,27 @@ export const Route = createFileRoute("/app/docs/new")({
 });
 
 function RouteComponent() {
-  const [md, setMd] = createSignal([
+  const [md, setMd] = createStore([
     `# Title 1\n## Title 2`,
     `# Title 1\n## Title 2`,
   ]);
-  const [html] = createResource(md, (md) =>
-    Promise.all(md.map((md) => marked.parse(md))),
+  const [html] = createResource(
+    () => md.slice(),
+    (md) => Promise.all(md.map((md) => marked.parse(md))),
   );
 
   return (
     <PageLayout title="New Doc">
       <div class="flex h-full flex-col gap-4">
-        <Index each={md()}>
+        {/* eslint-disable solid/no-innerhtml */}
+        <Index each={md}>
           {(item, index) => (
             <div class="flex gap-4">
               <Textarea
                 class="flex-1"
                 value={item()}
-                onInput={(md) =>
-                  setMd((v) => {
-                    const r = [...v];
-                    r[index] = md.currentTarget.value;
-                    return r;
-                  })
-                }
+                onInput={(md) => setMd(index, md.currentTarget.value)}
               />
-              {/* eslint-disable solid/no-innerhtml */}
               <div
                 innerHTML={html()?.[index] ?? ""}
                 class="prose flex-1 rounded-lg border-2 p-4"
@@ -42,6 +39,9 @@ function RouteComponent() {
             </div>
           )}
         </Index>
+        <Button variant="default" onClick={() => setMd((v) => [...v, ""])}>
+          Add
+        </Button>
       </div>
     </PageLayout>
   );
