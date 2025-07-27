25878cd9ef50c5530cb5224a823b3539c23fc38c
Prateek Kumar <me@prateek.page>
2025-07-27 09:52:14 +0400

    twopi-ui: account detail page

---
+++ twopi-ui/src/components/CurrencyDisplay.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value: number;
+  currencyCode: string;
+  decimalDigits: number;
+}>();
+
+const amount = computed(() => props.value / Math.pow(10, props.decimalDigits));
+</script>
+
+<template>
+  <div>
+    {{
+      Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: props.currencyCode,
+      }).format(amount)
+    }}
+  </div>
+</template>
--- twopi-ui/src/pages/app/finance/account/[id].detail.vue
+++ twopi-ui/src/pages/app/finance/account/[id].detail.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { useAccountsQuery } from "@/lib/account";
+import dayjs from "dayjs";
+
+const { data: accounts, state: accountsState } = useAccountsQuery();
+const route = useRoute();
+const params = route.params as { id: string };
+
+const account = computed(() => {
+  return params.id ? accounts.value?.accounts.find((c) => c.account.id === params.id) : undefined;
+});
+</script>
+
 <template>
   <AppPage title="Account Details">
     <template #actions>
@@ -5,5 +18,38 @@
         <UIcon name="i-lucide-arrow-left" /> All
       </ULink>
     </template>
+    <div v-if="accountsState.status === 'pending'">
+      <p>Loading category...</p>
+    </div>
+    <div v-else-if="account">
+      <LabelAndValue label="Id">{{ account.account.id }}</LabelAndValue>
+      <LabelAndValue label="Name">{{ account.account.name }}</LabelAndValue>
+      <LabelAndValue label="Account type">{{ account.account.account_extra }}</LabelAndValue>
+      <LabelAndValue label="Created at">
+        {{ dayjs(account.account.created_at).format("MMM D, YYYY h:mm A") }}
+      </LabelAndValue>
+      <LabelAndValue label="Currency">{{ account.currency.code }}</LabelAndValue>
+      <LabelAndValue label="Starting balance">
+        <UBadge
+          :color="
+            account.account.starting_balance < 0
+              ? 'error'
+              : account.account.starting_balance > 0
+                ? 'success'
+                : 'neutral'
+          "
+        >
+          <CurrencyDisplay
+            :value="account.account.starting_balance"
+            :currency-code="account.currency.code"
+            :decimal-digits="account.currency.decimal_digits"
+          />
+        </UBadge>
+      </LabelAndValue>
+      <LabelAndValue label="Cash flow">{{
+        account.account.is_cash_flow ? "Yes" : "No"
+      }}</LabelAndValue>
+      <LabelAndValue label="Active">{{ account.account.is_active ? "Yes" : "No" }}</LabelAndValue>
+    </div>
   </AppPage>
 </template>
