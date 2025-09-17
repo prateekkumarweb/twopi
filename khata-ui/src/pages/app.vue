e2a72637d5e38f5ee45469bcb6ec6ff39264343d
Prateek Kumar <me@prateek.page>
2025-09-17 19:38:55 +0400

    update icon

--- khata-ui/src/pages/app.vue
+++ khata-ui/src/pages/app.vue
@@ -87,7 +87,7 @@ const items: NavigationMenuItem[] = [
   <div v-if="user" class="relative flex h-screen flex-col">
     <header class="border-b-accented flex w-full gap-4 border-b p-4">
       <RouterLink to="/app" class="flex items-center gap-2">
-        <img src="/2pi.svg" alt="Khata" class="h-8 w-8" />
+        <img src="/khata.svg" alt="Khata" class="h-8 w-8" />
         <h1 class="flex-1 text-xl font-semibold">Khata</h1>
       </RouterLink>
       <div class="flex-1"></div>
